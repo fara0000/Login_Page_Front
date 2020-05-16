a388f91b3eb165cae0a7491325c47bf96b3db8d4
rauf-babayev <rauf.rb.business@gmail.com>
2020-05-16 21:44:49 +0400

    modal window done

--- src/View.js
+++ src/View.js
@@ -1,6 +1,13 @@
+import modalWindow from './ModalWindow';
+
 class View {
     constructor () {
         this._root = document.querySelector('div#root');
     }
+
+    init = () => {
+        modalWindow.init();
+    }
 }
+
 export default View;
