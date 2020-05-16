ba30ee9a453e1ff159720e2fae9d42721b949493
SevincMammadova <sevincmammadova609@gmail.com>
2020-05-16 21:52:16 +0400

    done

--- src/Controller.js
+++ src/Controller.js
@@ -2,10 +2,27 @@ class Controller {
     constructor( model,view ) {
         this._model = model;
         this._view = view;
-        
+        this.data = null;
     }
     init = () => {
         
+    }
+
+    init =()=> {
+        this._view.init();
+        this._view.getDataFromInput(this.getInputValue.bind(this));
+        
+    }
+
+    getInputValue = person =>{
+        if (!person) {
+            return;
+        }
+        
+        this.data = this._model.getDataFromModel();
+        this.data.push(person);
+       
+       console.log(this.data);
     }
 }
 export default Controller;
