7439525e8b651064b972fcab40dbfe5d794a1895
elnurmagerramli23 <61136502+elnurmagerramli23@users.noreply.github.com>
2020-05-17 12:28:12 +0400

    Lp 2 registration page (#10)
    * done
    
    * fix
    
    * LP-2 Create registration page
    
    * LP-2 Create registration page
    
    * LP-2 Registration page fixed
    
    Co-authored-by: SevincMammadova <sevincmammadova609@gmail.com>
    Co-authored-by: elchin-imanov <eemanovcoop@gmail.com>
    Co-authored-by: rauf-babayev <61136400+rauf-babayev@users.noreply.github.com>

--- src/Controller.js
+++ src/Controller.js
@@ -2,10 +2,17 @@ class Controller {
     constructor( model,view ) {
         this._model = model;
         this._view = view;
-        
+        this.data = null;
     }
+
     init = () => {
         this._view.init();
+        this._view.getDataFromInput(this.checkInsertedData.bind(this));
+        
+    }
+
+    checkInsertedData = person =>{
+
     }
 }
 export default Controller;
--- src/View.js
+++ src/View.js
@@ -1,8 +1,17 @@
+import Regpage from './RegPage';
 import ModalWindow from './pages/components/ModalWindow';
 
 class View {
     constructor () {
         this._root = document.querySelector('div#root');
+}
+
+init = () => {
+    this.regpage();
+}
+
+regpage = () => {
+    const regpage = new Regpage();
     }
 
     init = () => {
