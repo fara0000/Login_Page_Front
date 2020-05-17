dcb2ba43dd16ee01266815ddfac7f4b5a4eaa13e
SevincMammadova <sevincmammadova609@gmail.com>
2020-05-17 17:13:13 +0400

    LP-5-take_data_from_inputs

--- src/View.js
+++ src/View.js
@@ -2,35 +2,14 @@ class View {
     constructor () {
         this._root = document.querySelector('div#root');
         this.nameInput = null;
-        this.mailInput = null;
+        this.emailInput = null;
         this.passwordInput = null;
-        this.confirmPassInput = null;
+        this.confirmPassword = null;
         this.button = null;
     }
 
     init = () => {
-        const wrapper = document.createElement('div');
-        wrapper.className = 'wrapper';
-        this.nameInput = document.createElement('input');
-        this.mailInput = document.createElement('input');
-        this.passwordInput = document.createElement('input');
-        this.confirmPassInput = document.createElement('input');
-        this.nameInput.className = 'wrapper__nameInputField';
-        this.mailInput.className = 'wrapper__mailInputField';
-        this.passwordInput.className = 'wrapper__passwordInputField';
-        this.passwordInput.type = "password";
-        this.confirmPassInput.className = 'wrapper__confirmPassInputField';
-        this.confirmPassInput.type = "password";
-        this.button = document.createElement('button');
-        this.button.className = 'wrapper__button';
-        this.button.innerHTML = 'Send!';
-
-        wrapper.append(this.nameInput);
-        wrapper.append(this.mailInput);
-        wrapper.append(this.passwordInput);
-        wrapper.append(this.confirmPassInput);
-        wrapper.append(this.button);
-        this._root.append(wrapper);
+        
     }
 
     getDataFromInput = cb =>{
@@ -38,9 +17,9 @@ class View {
 
             const person = {
                 name: this.nameInput.value,
-                mail: this.mailInput.value,
+                email: this.emailInput.value,
                 password: this.passwordInput.value,
-                confirmPassword: this.confirmPassInput.value,
+                confirmPassword: this.confirmPassword.value,
             }
 
             cb(person);
